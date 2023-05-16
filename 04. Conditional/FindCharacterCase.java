a63a4e516d4f9829a9e402f91096c59aac6e9124
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-05-16 23:14:03 +0530

    Practice

---
+++ 04. Conditional/FindCharacterCase.java
@@ -0,0 +1,50 @@
+/* Find Character Case
+
+Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z)
+0, if the character is a lowercase alphabet (a - z)
+-1, if the character is not an alphabet
+Input format 
+Single Character
+Output format 
+1 or 0 or -1
+Constraints 
+Input can be any character
+Sample Input 1 
+v
+Sample Output 1 
+0
+Sample Input 2 
+V
+Sample Output 2 
+1
+Sample Input 3 
+#
+Sample Output 3 
+-1 
+*/
+
+import java.util.*;
+class FindCharacterCase{
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            char c = scan.next().charAt(0);
+            
+            // String str = scan.nextLine();
+            // char ch = str.charAt(0);
+            
+            if(c>='A' && c<='Z'){
+                System.out.println(1);
+            }
+            else if(c>='a' && c<='z'){
+                System.out.println(0);
+            }
+            else{
+                System.out.println(-1);
+            }
+        }
+
+
+    }
+}
+
